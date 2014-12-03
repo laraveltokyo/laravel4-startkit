aa891ba855d4871687a85de02df52811807c342a
Fumio Furukawa <fumio@straight-spirits.com>
2014-12-03 17:06:14 +0900

    specsの設定例を追加。

---
+++ app/lang/ja/forms.php
@@ -0,0 +1,31 @@
+<?php
+
+// MEMO: キー名はspecs/forms.phpと連動させること
+return [
+	// ログインフォームのテキスト
+	'login' => [
+		'rules' => '@rules',
+		// フォーム固有のラベル
+		'attributes' => [
+			'username' => 'ユーザー名',
+			'password' => 'パスワード',
+		],
+		// フォーム固有のヘルプテキスト
+		'helptexts' => [
+			'password' => '6文字以上',
+		],
+	],
+
+	// 例のテキスト
+	'example' => [	
+		'rules' => '@rules',
+		// フォーム固有のラベル
+		'attributes' => [
+			'title' => 'タイトル',
+			'subtitle' => 'サブタイトル',
+		],
+		// フォーム固有のヘルプテキスト
+		'helptexts' => [
+		],
+	],
+];
