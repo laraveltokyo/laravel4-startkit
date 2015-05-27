c4f5633dd30cfe77a435ea7b7996b4637397923a
Fumio Furukawa <jumilla@laravel.tokyo>
2015-05-28 06:03:33 +0900

    Update.

--- app/start/global.php
+++ app/start/global.php
@@ -86,3 +86,18 @@ App::down(function()
 */
 
 require app_path().'/filters.php';
+
+
+
+// Make sqlite database file
+if (Config::get('database.default') == 'sqlite') {
+	// check '{{env}}.sqlite' exists
+	$databaseFile = Config::get('database.connections.sqlite.database');
+
+	if (!file_exists($databaseFile)) {
+		// touch 'local.sqlite'
+		Log::info('Make Sqlite File "'.$databaseFile.'"');
+		file_put_contents($databaseFile, '');
+	}
+}
+
--- app/start/local.php
+++ app/start/local.php
@@ -1,12 +1,4 @@
 <?php
 
-if (Config::get('database.default') == 'sqlite') {
-	// check '{{env}}.sqlite' exists
-	$databaseFile = Config::get('database.connections.sqlite.database');
-
-	if (!file_exists($databaseFile)) {
-		// touch 'local.sqlite'
-		Log::info('Make Sqlite File "'.$databaseFile.'"');
-		file_put_contents($databaseFile, '');
-	}
-}
+// Add 'generate:*' commands
+app()->register('Way\Generators\GeneratorsServiceProvider');
